ccdfb81e079d247beecc7d24e9ce52f46e2a56d5
Jpardinas <jpardinas23@gmail.com>
2020-01-04 23:09:42 +0100

    eureka-service implemented

--- springboot-servicio-item/src/main/java/com/jpardinas/microservicios/springboot/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/jpardinas/microservicios/springboot/item/controllers/ItemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpardinas.microservicios.springboot.item.models.Item;
+import com.jpardinas.microservicios.springboot.item.models.Producto;
 import com.jpardinas.microservicios.springboot.item.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
@@ -29,6 +30,19 @@ public class ItemController {
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		
+		return item;
 	}
 
 }
