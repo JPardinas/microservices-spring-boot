3970f26f40431710ebc86e8e9f13b63f77391f4c
Jpardinas <jpardinas23@gmail.com>
2020-01-05 13:24:48 +0100

    config server

--- springboot-servicio-item/src/main/java/com/jpardinas/microservicios/springboot/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/jpardinas/microservicios/springboot/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.jpardinas.microservicios.springboot.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +24,14 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class ItemController {
 	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -43,6 +55,18 @@ public class ItemController {
 		item.setProducto(producto);
 		
 		return item;
+	}
+	
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
